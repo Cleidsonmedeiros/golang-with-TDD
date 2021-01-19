0a57cd77883c30035786a635b6e98bd8220e0a05
Cleidsonmedeiros <cleidsonmedeiros12@gmail.com>
2021-01-19 15:23:59 -0300

    Add files via upload

---
+++ Golang/language.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+const espanhol = "espanhol"
+const frances = "frances"
+const prefixenglish = "Hello, "
+const prefixespanhol = "Hola, "
+const prefixfrances = "Bonjour ,"
+
+func Hello(name string, language string) string {
+	if name == "" {
+		name = "World"
+	}
+
+	return prefixsalutation(language) + name
+}
+
+func prefixsalutation(language string) (prefix string) {
+	switch language {
+	case frances:
+		prefix = prefixfrances
+	case espanhol:
+		prefix = prefixespanhol
+	default:
+		prefix = prefixenglish
+	}
+	return
+}
+
+func main() {
+	fmt.Println(Hello("world", ""))
+}
